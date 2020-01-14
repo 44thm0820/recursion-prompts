bc85ca465e3ba9b50e17bc3d6adf493092c82cd0
Rey Mercado <reymercado.usa@gmail.com>
2020-01-13 23:04:33 -0800

    completed #31 lettertally

--- src/recursion.js
+++ src/recursion.js
@@ -531,6 +531,18 @@ var flatten = function(array) {
 // 31. Given a string, return an object containing tallies of each letter.
 // letterTally('potato'); // {p:1, o:2, t:2, a:1}
 var letterTally = function(str, obj) {
+  if (str.length === 1 && typeof obj !== 'object') {
+    obj = {}
+    obj[str[0]] = 1;
+  } else if (str.length === 1 && typeof obj === 'object' && !obj[str[0]]) {
+    obj[str[0]] = 1;
+  } else if (str.length === 1 && typeof obj === 'object' && obj[str[0]]) {
+    obj[str[0]] += 1;
+  } else {
+    obj = letterTally(str.slice(1), letterTally(str.slice(0,1),obj));
+  }
+  return obj;
+
 };
 
 // 32. Eliminate consecutive duplicates in a list. If the list contains repeated
