99a332ca510371b7e1ba6134f19026842aaa936a
Rey Mercado <reymercado.usa@gmail.com>
2020-01-14 18:37:16 -0800

    completed #10 palindrome

--- src/recursion.js
+++ src/recursion.js
@@ -129,6 +129,12 @@ var reverse = function(string) {
 
 // 10. Write a function that determines if a string is a palindrome.
 var palindrome = function(string) {
+  string = string.toLowerCase().split(' ').join('');
+  if(string.length === 0 || string.length === 1) {
+    return true;
+  } else {
+    return (string[0] === string[string.length-1]) && palindrome(string.slice(1,-1));
+  }
 };
 
 // 11. Write a function that returns the remainder of x divided by y without using the
